265222de49ca500b9ce8157e6dcacaeee0a580b3
RIN <97146986+JIN-HR@users.noreply.github.com>
2024-09-02 15:58:31 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -45,7 +45,7 @@ class _CaptioningAppState extends State<CaptioningApp> {
     if (_imageData == null) return; // 예외 처리 (실패 시 안내 메시지 표출)
 
     try {
-      final uri = Uri.parse("http://YOUR_FLASK_SERVER_IP:5000/generate_caption");
+      final uri = Uri.parse("http://10.0.2.2:5000/generate_caption");
 
       var request = http.MultipartRequest('POST', uri);
       var multipartFile = http.MultipartFile.fromBytes('image', _imageData!, filename: 'upload.jpg');
